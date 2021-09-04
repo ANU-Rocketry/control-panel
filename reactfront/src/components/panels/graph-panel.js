3d5a6b7c76bcf73792b7bded7658df20f65e5222
oliver-bagin <oliverbagin1@outlook.com>
2021-09-04 18:46:16 +1000

    graph

---
+++ reactfront/src/components/panels/graph-panel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Panel } from '../index'
+
+export default function SafetyPanel({ state, emit }) {
+
+  const lox8Active = state.data === null ? 0 : state.data["LOX"]["analog"]["4"]
+
+  return (
+    <Panel title="Graphs">
+      
+    </Panel>
+  )
+}
