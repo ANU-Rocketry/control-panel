05f6039c6b0d6f55327bd869d9c470d9e3b52430
OliverBalfour <oliver.leo.balfour@gmail.com>
2021-09-04 16:43:07 +1000

    refactor

--- reactfront/src/components/Safety/safety-panel.js
+++ reactfront/src/components/Safety/safety-panel.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import SafetyCard from './safety-card';
-import { Panel } from '../index'
+import { Panel, ToggleSwitch } from '../index'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+function SafetyCard(props) {
+  return (
+    <Card>
+      <CardContent>
+        <h2>
+          {props.title}
+        </h2>
+        <Typography color="textSecondary">
+          {props.label}
+        </Typography>
+        <ToggleSwitch value={props.switchValue} setValue={props.setSwitchValue}/>
+      </CardContent>
+    </Card>
+  );
+}
 
 export default function SafetyPanel({ state, emit }) {
   // TODO: backend should have a boolean param for arming switch instead of toggling
   const armingSwitchActive = state.data === null ? false : state.data.arming_switch
-  const toggleArmingSwitch = () => emit('ARMINGSWITCH', !armingSwitchActive)
+  const toggleArmingSwitch = x => emit('ARMINGSWITCH', x)
   return (
     <Panel title="Safety">
       <SafetyCard title="Arming Switch"
         label="Switch Controlling The Arming"
-        switchState={armingSwitchActive}
-        switchFunction={toggleArmingSwitch}/>
+        switchValue={armingSwitchActive}
+        setSwitchValue={toggleArmingSwitch}/>
     </Panel>
   )
 }
--- reactfront/src/components/index.js
+++ reactfront/src/components/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Paper, Typography, Toolbar, AppBar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
+import Switch from '@material-ui/core/Switch';
+import Grid from '@material-ui/core/Grid';
 
 export function SectionTitle({ children }) {
     return (
@@ -52,3 +55,50 @@ export function Panel({ children, title }) {
   );
 }
 
+export const CustomSwitch = withStyles((theme) => ({
+  root: {
+    width: 28,
+    height: 16,
+    padding: 0,
+    display: 'flex',
+  },
+  switchBase: {
+    padding: 2,
+    color: theme.palette.grey[500],
+    '&$checked': {
+      transform: 'translateX(12px)',
+      color: theme.palette.common.white,
+      '& + $track': {
+        opacity: 1,
+        backgroundColor: theme.palette.primary.main,
+        borderColor: theme.palette.primary.main,
+      },
+    },
+  },
+  thumb: {
+    width: 12,
+    height: 12,
+    boxShadow: 'none',
+  },
+  track: {
+    border: `1px solid ${theme.palette.grey[500]}`,
+    borderRadius: 16 / 2,
+    opacity: 1,
+    backgroundColor: theme.palette.common.white,
+  },
+  checked: {},
+}))(Switch);
+
+export function ToggleSwitch({ value, setValue }) {
+  return (
+    <Typography component="div">
+      <Grid component="label" container alignItems="center" spacing={1}>
+        <Grid item>Off</Grid>
+        <Grid item>
+          <CustomSwitch checked={value} onChange={() => setValue(!value)} />
+        </Grid>
+        <Grid item>On</Grid>
+      </Grid>
+    </Typography>
+  );
+}
