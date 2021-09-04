2cae7628dc373f31e38f1d8ff410d06ec7275c5e
OliverBalfour <oliver.leo.balfour@gmail.com>
2021-09-04 13:40:42 +1000

    improve server

--- server.py
+++ server.py
@@ -8,8 +8,7 @@ from lib.LJCommands import *
 # Importing from fake labjack so we can test the software
 from lib.LabJackFake import LabJack
 
-
-class LJSocketWebSockets:
+class LJWebSocketsServer:
 
     def __init__(self, ip: str, port: int, **kwargs):
         self.labjacks = kwargs
@@ -35,14 +34,25 @@ class LJSocketWebSockets:
         for task in pending:
             task.cancel()
 
-    async def consumer_handler(self, websocket, path):
-        async for message in websocket:
-            await self.consumer(message)
+    async def consumer_handler(self, ws, path):
+        async for message in ws:
+            data = json.loads(message)
+            if 'command' in data.keys():
+                await self.handle_command(ws, data['command']['header'],
+                    data['command'].get('data', None), data['time'])
 
-    async def producer_handler(self, websocket, path):
+    async def handle_command(self, ws, header, data, time):
+        if header != CommandString.PING.value: print(header)
+
+        if header == CommandString.PING.value:
+            await self.emit(ws, 'PING', time)
+        elif header == CommandString.ARMINGSWITCH.value:
+            self.execute(Command(CommandString.ARMINGSWITCH))
+
+    async def producer_handler(self, ws, path):
         while True:
             await asyncio.sleep(1/20)
-            await self.emit('STATE', self.state)
+            await self.emit(ws, 'STATE', self.state)
 
     def start_server(self):
         start_server = websockets.serve(self.event_handler, self.ip, self.port)
@@ -50,34 +60,24 @@ class LJSocketWebSockets:
         asyncio.get_event_loop().run_forever()
 
     def execute(self, command: Command):
+        # TODO: arming switch shouldn't be a toggle, it should take a bool
         if command.header == CommandString.ARMINGSWITCH:
             self.state['arming_switch'] = not self.state['arming_switch']
             print(self.state['arming_switch'])
         else:
             print(command)
 
-    async def consumer(self, data):
-        jData = json.loads(data)
-        if 'command' in jData.keys():
-            if jData["command"]["header"] == CommandString.PING.value:
-                await self.emit('PING', jData['time'])
-                print("PING")
-            elif jData["command"]["header"] == CommandString.ARMINGSWITCH.value:
-                self.execute(Command(CommandString.ARMINGSWITCH))
-                print("ARMINGSWITCH")
-
-    async def emit(self, msg_type, data):
+    async def emit(self, ws, msg_type, data):
         # if msg_type == "STATE", data is the state, etc.
         obj = {
             "type": msg_type,
             "time": round(time.time()*1000),
             "data": data
         }
-        await self.websocket.send(json.dumps(obj))
-
+        await ws.send(json.dumps(obj))
 
 if __name__ == '__main__':
     ip = "127.0.0.1"
     port = 8888
-    socket = LJSocketWebSockets(ip, port, lox=LabJack(1), eth=LabJack(2))
+    socket = LJWebSocketsServer(ip, port, lox=LabJack(1), eth=LabJack(2))
     socket.start_server()
