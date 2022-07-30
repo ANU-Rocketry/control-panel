4dd8e13398a33950627bacb461ada868411b3614
Oliver Balfour <oliver.leo.balfour@gmail.com>
2022-07-30 18:26:39 +1000

    shuffle code around

--- src/commands.py
+++ src/commands.py
@@ -3,18 +3,19 @@
 #all of the computation is done through the field "act"
 
 from abc import ABCMeta, abstractmethod
+from xmlrpc.client import Server
 from stands import Stand, StandConfig
 import time
 import asyncio
-
-def time_ms():
-    return time.time_ns() // 1_000_000
+from dataclasses import dataclass, asdict
+import json
+import utils
 
 # Commands that the server executes to control the hardware
 # Changing pins manually will dispatch a client command which creates a server command
 # Creating a sequence creates a list of server commands
 # Types include open, close, sleep
-class Command(metaclass=ABCMeta):
+class ServerCommand(metaclass=ABCMeta):
     name: str
     @abstractmethod
     async def act(self, server):
@@ -25,7 +26,7 @@ class Command(metaclass=ABCMeta):
     def __str__(self):
         return self.name
 
-class Sleep(Command):
+class Sleep(ServerCommand):
     def __init__(self, ms: int):
         self.name = "SLEEP"
         self.ms = ms
@@ -37,17 +38,17 @@ class Sleep(Command):
 
         print(f"[Sequence] sleeping for {self.ms}ms")
         # break sleep into 5ms chunks so aborts will interrupt sleeps
-        expiry = time_ms() + self.ms # milliseconds
-        while time_ms() < expiry:
+        expiry = utils.time_ms() + self.ms # milliseconds
+        while utils.time_ms() < expiry:
             if server.should_interrupt_sleep(): break
             await asyncio.sleep(0.005) # 5ms
         # update remaining time if we're pausing so that we can continue by calling act again
-        self.ms = max(0, expiry - time_ms())
+        self.ms = max(0, expiry - utils.time_ms())
 
     def as_dict(self):
         return { "name": self.name, "ms": self.ms }
 
-class Open(Command):
+class Open(ServerCommand):
     def __init__(self, stand: Stand, pin: int):
         self.name = "OPEN"
         self.stand = stand
@@ -57,7 +58,7 @@ class Open(Command):
     def as_dict(self):
         return { "name": self.name, "stand": self.stand, "pin": self.pin }
 
-class Close(Command):
+class Close(ServerCommand):
     def __init__(self, stand: Stand, pin: int):
         self.name = "CLOSE"
         self.stand = stand
@@ -67,4 +68,20 @@ class Close(Command):
     def as_dict(self):
         return { "name": self.name, "stand": self.stand, "pin": self.pin }
 
+@dataclass
+class ClientCommand:
+    name: str
+    pin: int = None # for OPEN, CLOSE
+    stand: Stand = None # for OPEN, CLOSE
+    ms: int = None # for SLEEP
+    sequence_file: str = None # for SETSEQUENCE
+    enabled: bool = None # for ARMINGSWITCH, MANUALSWITCH, DATALOG
+
+    def as_dict(self) -> dict:
+        return asdict(self)
+
+    def __str__(self) -> str:
+        return json.dumps(self.as_dict())
+
 #parser: map(eval, string.split("\n")) #check new line
+
